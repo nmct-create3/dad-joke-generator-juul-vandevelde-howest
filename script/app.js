e2d23f815541f4174425a668bedb3b186ef6a976
juul-vandevelde-howest <juul.van.de.velde@student.howest.be>
2023-10-11 10:18:26 +0200

    begin oef dad joke

--- script/app.js
+++ script/app.js
@@ -1,5 +1,48 @@
+const jokeContent = document.querySelector('.js-joke');
+const jokeBtn = document.querySelector('.js-random-joke');
+const jokeLoader = document.querySelector('.js-loading-state');
+let loadingDelayed;
+
+const showLoader = function () {
+  loadingDelayed = setTimeout(() => {
+    jokeLoader.classList.remove('u-hidden');
+  }, 500);
+  jokeContent.classList.add('u-hidden');
+};
+
+const removeLoader = function () {
+  if (loadingDelayed) {
+    clearTimeout(loadingDelayed);
+    loadingDelayed = null;
+  }
+
+  jokeLoader.classList.add('u-hidden');
+  jokeContent.classList.remove('u-hidden');
+};
+
+const fetchJoke = function () {
+  return fetch('https://icanhazdadjoke.com/', {
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.error('Error fetching joke:', error);
+    });
+};
+
+const generateJoke = async () => {
+  showLoader();
+  const { joke } = await fetchJoke();
+  jokeContent.innerHTML = joke;
+  removeLoader();
+};
+
 const init = function () {
-    console.log('script loaded');
-}
+  console.log('script loaded');
+  generateJoke();
+  jokeBtn.addEventListener('click', generateJoke);
+};
 
 init();
